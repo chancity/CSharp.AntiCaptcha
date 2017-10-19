2d1b0aa92544576a7be5291979f38fa0b1173521
Chancey <hawbakerchancey@gmail.com>
2017-10-19 11:01:53 -0700

    fixed title

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("fukyousam")]
+[assembly: AssemblyTitle("AntiCaptcha")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("fukyousam")]
+[assembly: AssemblyProduct("AntiCaptcha")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
